24af6f4ca9882436320153d67af7464d948e74a4
Jocelyn1812 <221137@utags.edu.mx>
2025-09-24 22:35:19 -0600

    Introduce error grave para probar rollback

--- app/index.js
+++ app/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// ERROR GRAVE - línea incompleta sin paréntesis
 app.get('/', (req, res) => {
-  res.send('¡Hola desde CI/CD!');
-});
+    res.send('¡Hola desde CI/CD!')
+// ← FALTÓ el punto y coma y paréntesis de cierre
 
 app.listen(port, () => {
-  console.log(`App escuchando en http://localhost:${port}`);
-}
+    console.log('App escuchando en http://localhost:${port}');
+});
